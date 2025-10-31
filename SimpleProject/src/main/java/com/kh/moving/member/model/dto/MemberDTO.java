b7e6dc095ed63624ece4db0d3a825f8f405c24e0
dltbxkfm86-creator <dltbxkfm86@gmail.com>
2025-10-31 09:04:37 +0900

    login

--- SimpleProject/src/main/java/com/kh/moving/member/model/dto/MemberDTO.java
+++ SimpleProject/src/main/java/com/kh/moving/member/model/dto/MemberDTO.java
@@ -1,5 +1,30 @@
 package com.kh.moving.member.model.dto;
 
-public class MemberDTO {
+import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter 
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MemberDTO {
+	
+	private int userNo;
+    private String userId;
+    private String userPwd;
+    private String userName;
+    private String birthday;
+    private String email;
+    private String phone;
+    private String snsAgree;
+    private Date erollDate;
+    private String manager;
+    private String status;
+	
 }
--- SimpleProject/src/main/java/com/kh/moving/member/model/service/MemberService.java
+++ SimpleProject/src/main/java/com/kh/moving/member/model/service/MemberService.java
@@ -1,5 +1,18 @@
 package com.kh.moving.member.model.service;
 
-public class MemberService {
+import javax.servlet.http.HttpSession;
 
+import com.kh.moving.member.model.dto.MemberDTO;
+
+public interface MemberService {
+	
+	MemberDTO login(MemberDTO member);
+
+	void signUp(MemberDTO member);
+
+	void update(MemberDTO member, HttpSession session);
+
+	void delete(String userPwd, HttpSession session);
+	
 }
+
