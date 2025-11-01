4c857e6743086af2a401164e891f824d44adb743
kanghyunseong <gustjd2754@daum.net>
2025-11-02 01:57:07 +0900

    123

--- SimpleProject/src/main/java/com/kh/moving/notice/controller/NoticeController.java
+++ SimpleProject/src/main/java/com/kh/moving/notice/controller/NoticeController.java
@@ -1,5 +1,6 @@
 package com.kh.moving.notice.controller;
 
+
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -53,6 +54,48 @@ public class NoticeController {
     public String noticeDetail(@RequestParam("noticeId") int noticeId, Model model) {
         return "notice/notice_Detail";
     }
+	
+	// 비동기 방식 시작
+	
+	/*
+	 * 공지사항 목록을 반환 받기 
+	 */
+	
+	/*
+	@GetMapping("/noticeList")
+	public String noticeListPage() {
+		// log.info("공지사항 목록 반환");
+		return "notice/notice_List";
+	}
+	
+	// 공지사항 목록을 AJAX요청으로 비동기 방식으로 받
+	@GetMapping(value = "/list/data", produces = "application/json; charset=UTF-8")
+	@ResponseBody
+    public String getNoticeList(@RequestParam(value="page", defaultValue="1") int currentPage) {
+    	
+		// log.info("AJAX 요청 오는지 확인 : {} ", currentPage);
+		
+		int listCount = noticeService.getNoticeCount();
+		PageInfo pageInfo = pagenation.getPageInfo(listCount, currentPage, 10, 5);
+		List<NoticeDTO> noticeList = noticeService.findAllNotice(pageInfo);
+		
+		JsonObject resposneData = new JsonObject();
+		JsonArray noticeListArray = new JsonArray();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+		
+		
+		
+		return null;
+    }
+    */
+    
+    // 페이징 처리 끝(게시글 목록 조회)
+    
+    // 게시글 글쓰기 
+    // 권한은 관리자 status = Y인 사람한테만 권한주기 
+    
+    
     
     
 }
