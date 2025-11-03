f4c0a98dcb9a05e793d74ca89dad2d4148118a3f
yakgom <dirrha123@naver.com>
2025-11-03 17:38:06 +0900

    20251103 commit

--- SimpleProject/src/main/java/com/kh/moving/member/model/dao/MemberMapper.java
+++ SimpleProject/src/main/java/com/kh/moving/member/model/dao/MemberMapper.java
@@ -12,17 +12,17 @@ import com.kh.moving.member.model.dto.MemberGenreDTO;
 @Mapper
 public interface MemberMapper {
 	
-	@Select("SELECT USER_ID userId, USER_PWD userPwd, USER_NAME userName, BIRTHDAY, PHONE, EMAIL, ENROLL_DATE enrollDate, SNS_AGREE snsAgree, PREFERRED_GENRES preferredGenres, ENROLL_DATE, MANAGER, STATUS FROM TB_MEMBER WHERE USER_ID = #{userId}")
+	@Select("SELECT USER_ID userId, USER_PWD userPwd, USER_NAME userName, BIRTHDAY, PHONE, EMAIL, ENROLL_DATE enrollDate, SNS_AGREE snsAgree,  ENROLL_DATE, MANAGER, STATUS FROM TB_MEMBER WHERE USER_ID = #{userId}")
 	MemberDTO login(MemberDTO member);
 	
-	@Insert("INSERT INTO TB_MEMBER(USER_NO, USER_ID, USER_PWD, USER_NAME, BIRTHDAY, PHONE, EMAIL, SNS_AGREE, PREFERRED_GENRES) VALUES (SEQ_USERNO.NEXTVAL, #{userId}, #{userPwd}, #{userName}, #{birthday}, #{phone}, #{email}, #{snsAgree}, #{preferredGenres})")
+	@Insert("INSERT INTO TB_MEMBER(USER_NO, USER_ID, USER_PWD, USER_NAME, BIRTHDAY, PHONE, EMAIL, SNS_AGREE) VALUES (SEQ_USERNO.NEXTVAL, #{userId}, #{userPwd}, #{userName}, #{birthday}, #{phone}, #{email}, #{snsAgree})")
 
 	int signup(MemberDTO member);
 
 	@Insert("INSERT INTO TB_MEMBER_GENRE(USER_NO, GENRE_ID) VALUES (#{userNo}, #{genreId})")
 	int signup2(MemberGenreDTO genre);
 	
-	@Update("UPDATE TB_MEMBER SET USER_NAME = #{userName}, BIRTHDAY = #{birthday}, PHONE = #{phone}, EMAIL = #{email}, SNS_AGREE = #{snsAgree}, PREFERRED_GENRES = #{preferredGenres} WHERE USER_ID = #{userId}")
+	@Update("UPDATE TB_MEMBER SET USER_NAME = #{userName}, BIRTHDAY = #{birthday}, PHONE = #{phone}, EMAIL = #{email}, SNS_AGREE = #{snsAgree} WHERE USER_ID = #{userId}")
 
 	int update(MemberDTO member);
 	
